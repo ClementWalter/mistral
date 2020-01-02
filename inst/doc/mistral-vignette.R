8cabf6f94cc1ea7391c3714ccf2bf55b97d00673
Clément Walter <clementw@sicara.com>
2020-01-02 08:37:11 +0100

    Update gitignore

--- inst/doc/mistral-vignette.R
+++
@@ -1,8 +0,0 @@
-## ----init, cache=FALSE, include=FALSE------------------------------------
-library(knitr)
-knitr::opts_chunk$set(fig.path='figure/',
-               cache.path='cache/',
-               cache=TRUE)
-
-knitr::opts_knit$set(animation.fun = hook_scianimator)
-
